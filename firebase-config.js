5d2ebc04df85c9177730a7bf1711a0666aaaf0d5
coretrex <122108646+coretrex@users.noreply.github.com>
2024-09-12 10:54:22 -0500

    Firebase config

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCHQcAa3Z-ADIDeTI88LXgP4gmVPFuM9Hc",
+    authDomain: "coretrex-dashboard.firebaseapp.com",
+    projectId: "coretrex-dashboard",
+    storageBucket: "coretrex-dashboard.appspot.com",
+    messagingSenderId: "964784655965",
+    appId: "1:964784655965:web:c25a95fd4e4af26be407b4",
+    measurementId: "G-NLRD798V18"
+};
+firebase.initializeApp(firebaseConfig);
